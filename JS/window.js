a851b737444a9e37aa92f05f70d1e59534382e72
Marina Palma <marina_palma@hotmail.com>
2022-04-28 10:31:34 +0100

    github4

--- JS/window.js
+++ JS/window.js
@@ -4,30 +4,24 @@ const descriptionScr = document.getElementById("mainSc");
 const startScr = document.getElementById("startScr");
 const restartBt = document.getElementById("restart-button");
 const startBt = document.getElementById("start-button");
-const gameOverScreen = document.getElementById('game-over')
+const gameOverScreen = document.getElementById("game-over");
 const soundBeg = new Audio(
-  "./docs/assets/sounds/Star_Wars_original_opening_crawl_1977.mp3"
+  "docs/assets/sounds/Star_Wars_original_opening_crawl_1977.mp3"
 );
 
-
-
-
-
-
 function startGame() {
-startBt.style.display="none";
-restartBt.style.display="flex";
+  startBt.style.display = "none";
+  restartBt.style.display = "flex";
 
   if (!game) {
     game = new Space();
-    game.canvas.style.display="flex";
-    
+    game.canvas.style.display = "flex";
   }
   if (!game.active) {
     clearInterval(game.intervalId);
     game.ctx.clearRect(0, 0, game.width, game.height);
     game = new Space();
-    game.canvas.style.display="flex";
+    game.canvas.style.display = "flex";
     game.start();
     game.countTime();
   }
@@ -35,34 +29,24 @@ restartBt.style.display="flex";
 
 window.onload = () => {
   document.getElementById("start-button").onclick = () => {
+    soundBeg.play();
+    startScr.style.display = "none";
+    descriptionScr.style.display = "flex";
 
-  soundBeg.play();
-  startScr.style.display ="none";
-  descriptionScr.style.display="flex";
- 
-  setTimeout(()=> {
-    descriptionScr.style.display="none";
-    // canvasScr.style.display = "flex"
-    startGame();
-  }, 18000);
-
+    setTimeout(() => {
+      descriptionScr.style.display = "none";
+      // canvasScr.style.display = "flex"
+      startGame();
+    }, 18000);
   };
 
   document.getElementById("restart-button").onclick = () => {
     gameOverScreen.style.display = "none";
-    startScr.style.display ="none";
-    descriptionScr.style.display="none";
-  startGame();
-  
-    };
+    startScr.style.display = "none";
+    descriptionScr.style.display = "none";
+    startGame();
+  };
 };
-
-
-
-
-
-
-
 
 //  window.onload = () => {
 //   document.getElementById("start-button").onclick = () => {
@@ -76,5 +60,4 @@ window.onload = () => {
 //     game = new Space();
 //     game.start();
 //   }
-// }; 
-
+// };
